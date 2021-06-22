4038376bf6f07708a1a57d7aaf391737f7e9915d
Didier Vojtisek <didier.vojtisek@inria.fr>
2021-06-22 17:20:21 +0200

    base version of BPMN-Designer based on 6fc80f3
    base version:
    https://github.com/ObeoNetwork/BPMN-Designer/commit/6fc80f3a28b9d88f8663f8b85281ce9060405d2b
    
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

---
+++ plugins/org.obeonetwork.dsl.bpmn2.design/src/org/obeonetwork/bpmn2/design/ProcessService.java
@@ -0,0 +1,227 @@
+/**
+ * Copyright (c) 2011-2019 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ * 
+ */
+package org.obeonetwork.bpmn2.design;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.emf.cdo.eresource.CDOResource;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
+import org.obeonetwork.dsl.bpmn2.BoundaryEvent;
+import org.obeonetwork.dsl.bpmn2.Bpmn2Factory;
+import org.obeonetwork.dsl.bpmn2.ChoreographyTask;
+import org.obeonetwork.dsl.bpmn2.Collaboration;
+import org.obeonetwork.dsl.bpmn2.ComplexGateway;
+import org.obeonetwork.dsl.bpmn2.Definitions;
+import org.obeonetwork.dsl.bpmn2.EndEvent;
+import org.obeonetwork.dsl.bpmn2.EventBasedGateway;
+import org.obeonetwork.dsl.bpmn2.ExclusiveGateway;
+import org.obeonetwork.dsl.bpmn2.FlowElement;
+import org.obeonetwork.dsl.bpmn2.FlowElementsContainer;
+import org.obeonetwork.dsl.bpmn2.FlowNode;
+import org.obeonetwork.dsl.bpmn2.ImplicitThrowEvent;
+import org.obeonetwork.dsl.bpmn2.InclusiveGateway;
+import org.obeonetwork.dsl.bpmn2.InteractionNode;
+import org.obeonetwork.dsl.bpmn2.IntermediateCatchEvent;
+import org.obeonetwork.dsl.bpmn2.Lane;
+import org.obeonetwork.dsl.bpmn2.MessageFlow;
+import org.obeonetwork.dsl.bpmn2.ParallelGateway;
+import org.obeonetwork.dsl.bpmn2.Process;
+import org.obeonetwork.dsl.bpmn2.RootElement;
+import org.obeonetwork.dsl.bpmn2.SequenceFlow;
+import org.obeonetwork.dsl.bpmn2.StartEvent;
+import org.obeonetwork.dsl.bpmn2.SubProcess;
+import org.obeonetwork.dsl.bpmn2.Task;
+
+public class ProcessService {
+
+	public static void duplicate(Process process) {
+
+		// Clone the process
+		@SuppressWarnings("serial")
+		Copier copier = new Copier() {
+			@Override
+			protected void copyAttribute(EAttribute eAttribute, EObject eObject, EObject copyEObject) {
+				if (!eAttribute.isID()) {
+					super.copyAttribute(eAttribute, eObject, copyEObject);
+				}
+			}
+		};
+		Process clone = (Process) copier.copy(process);
+		clone.setName(clone.getName() + " (copy)");
+		copier.copyReferences();
+
+		// Clone message flows
+		Definitions defs = ServiceHelper.getDefinitionsObject(process);
+		for (RootElement root : defs.getRootElements()) {
+			if (root instanceof Collaboration) {
+				Collaboration collaboration = (Collaboration) root;
+				Collection<MessageFlow> newMFs = new ArrayList<MessageFlow>();
+				for (MessageFlow mf : collaboration.getMessageFlows()) {
+					InteractionNode sourceRef = mf.getSourceRef();
+					InteractionNode targetRef = mf.getTargetRef();
+					if (copier.containsKey(sourceRef) && copier.containsKey(targetRef)) {
+						MessageFlow messageFlowClone = (MessageFlow) copier.copy(mf);
+						mf.setSourceRef((InteractionNode) copier.get(sourceRef));
+						mf.setTargetRef((InteractionNode) copier.get(targetRef));
+						newMFs.add(messageFlowClone);
+					}
+				}
+				collaboration.getMessageFlows().addAll(newMFs);
+			}
+		}
+
+		// Save process clone
+		EObject container = process.eContainer();
+		if (container instanceof Definitions) {
+			((Definitions) container).getRootElements().add(clone);
+		} else {
+			((Process) container).getDecomposedBy().add(clone);
+		}
+	}
+
+	public static void paste(EObject container, EObject containerView, EObject copiedView, EObject copiedElement) {
+		// change IDs
+		EcoreUtil.setID(copiedElement, EcoreUtil.generateUUID());
+		for (Iterator<EObject> iterator = copiedElement.eAllContents(); iterator.hasNext();) {
+			EcoreUtil.setID(iterator.next(), EcoreUtil.generateUUID());
+		}
+
+		// change Name
+		EStructuralFeature nameFeature = copiedElement.eClass().getEStructuralFeature("name"); //$NON-NLS-1$
+		if (nameFeature != null) {
+			Object name = copiedElement.eGet(nameFeature);
+			if (name == null) {
+				name = "";
+			}
+			copiedElement.eSet(nameFeature, name + " (copy)"); // $NON-NLS-1$
+		}
+
+		// add to container
+		for (EReference containment : container.eClass().getEAllContainments()) {
+			if (containment.getEType().equals(copiedElement.eClass())
+					|| copiedElement.eClass().getEAllSuperTypes().contains(containment.getEType())) {
+				if (containment.isMany()) {
+					((EList<EObject>) container.eGet(containment)).add(copiedElement);
+				} else {
+					container.eSet(containment, copiedElement);
+				}
+			}
+		}
+	}
+
+	public EList<FlowElement> getSubElements(EObject eo) {
+		if (eo instanceof SubProcess) {
+			return ((SubProcess) eo).getFlowElements();
+		}
+		return null;
+	}
+
+	public boolean isApplicableType(EObject element) {
+		return isEventTaskOrGateway(element) || isLaneChoregraphyTaskOrSubProcess(element);
+	}
+
+	private boolean isEventTaskOrGateway(EObject element) {
+		return element instanceof BoundaryEvent || element instanceof ImplicitThrowEvent
+				|| element instanceof IntermediateCatchEvent || element instanceof EndEvent
+				|| element instanceof StartEvent || element instanceof Task || element instanceof ParallelGateway
+				|| element instanceof ExclusiveGateway || element instanceof InclusiveGateway
+				|| element instanceof ComplexGateway || element instanceof EventBasedGateway;
+	}
+
+	private boolean isLaneChoregraphyTaskOrSubProcess(EObject element) {
+		return element instanceof Lane || element instanceof ChoreographyTask || element instanceof SubProcess;
+	}
+
+	public EObject createFlowElement(EObject container, String typeToCreate) {
+		FlowElementsContainer flowElementsContainer = getFlowElementsContainer(container);
+		FlowNode newElement = createElement(typeToCreate);
+		flowElementsContainer.getFlowElements().add(newElement);
+
+		if (isEventTaskOrGateway(container)) {
+			List<Lane> lanes = ((FlowNode) container).getLanes();
+			newElement.getLanes().addAll(lanes);
+			SequenceFlow sequence = Bpmn2Factory.eINSTANCE.createSequenceFlow();
+			flowElementsContainer.getFlowElements().add(sequence);
+			sequence.setSourceRef((FlowNode) container);
+			sequence.setTargetRef(newElement);
+		} else if (isLaneChoregraphyTaskOrSubProcess(container)) {
+			if (container instanceof Lane) {
+				newElement.getLanes().add((Lane) container);
+			} else if (container instanceof ChoreographyTask) {
+				// TODO Nothing in the odesign migrated
+			} else if (container instanceof SubProcess) {
+				// TODO Nothing in the odesign migrated
+			}
+		}
+		return newElement;
+	}
+
+	private FlowElementsContainer getFlowElementsContainer(EObject eo) {
+		FlowElementsContainer result = null;
+		if (eo instanceof FlowElementsContainer) {
+			result = (FlowElementsContainer) eo;
+		} else if (eo.eContainer() != null && !(eo.eContainer() instanceof CDOResource)) {
+			result = getFlowElementsContainer(eo.eContainer());
+		}
+		return result;
+	}
+
+	private FlowNode createElement(String typeToCreate) {
+		FlowNode result = null;
+		if ("StartEvent".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createStartEvent();
+		} else if ("EndEvent".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createEndEvent();
+		} else if ("IntermediateCatchEvent".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createIntermediateCatchEvent();
+		} else if ("IntermediateThrowEvent".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createIntermediateThrowEvent();
+		} else if ("Task".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createTask();
+		} else if ("BusinessRuleTask".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createBusinessRuleTask();
+		} else if ("ManualTask".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createManualTask();
+		} else if ("ReceiveTask".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createReceiveTask();
+		} else if ("ScriptTask".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createScriptTask();
+		} else if ("SendTask".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createSendTask();
+		} else if ("ServiceTask".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createServiceTask();
+		} else if ("UserTask".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createUserTask();
+		} else if ("ParallelGateway".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createParallelGateway();
+		} else if ("ExclusiveGateway".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createExclusiveGateway();
+		} else if ("InclusiveGateway".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createInclusiveGateway();
+		} else if ("ComplexGateway".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createComplexGateway();
+		} else if ("EventBasedGateway".equals(typeToCreate)) {
+			result = Bpmn2Factory.eINSTANCE.createEventBasedGateway();
+		}
+		return result;
+	}
+
+}
---
+++ plugins/org.obeonetwork.dsl.bpmn2.design/src/org/obeonetwork/bpmn2/design/TaskService.java
@@ -0,0 +1,236 @@
+/**
+ * Copyright (c) 2011-2019 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ * 
+ */
+package org.obeonetwork.bpmn2.design;
+
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
+import org.eclipse.emf.ecore.util.ECrossReferenceAdapter;
+import org.eclipse.sirius.diagram.DNodeContainer;
+import org.obeonetwork.dsl.bpmn2.Association;
+import org.obeonetwork.dsl.bpmn2.Bpmn2Factory;
+import org.obeonetwork.dsl.bpmn2.Bpmn2Package;
+import org.obeonetwork.dsl.bpmn2.BusinessRuleTask;
+import org.obeonetwork.dsl.bpmn2.CallActivity;
+import org.obeonetwork.dsl.bpmn2.ChoreographyTask;
+import org.obeonetwork.dsl.bpmn2.FlowElementsContainer;
+import org.obeonetwork.dsl.bpmn2.GlobalBusinessRuleTask;
+import org.obeonetwork.dsl.bpmn2.GlobalManualTask;
+import org.obeonetwork.dsl.bpmn2.GlobalScriptTask;
+import org.obeonetwork.dsl.bpmn2.GlobalUserTask;
+import org.obeonetwork.dsl.bpmn2.ManualTask;
+import org.obeonetwork.dsl.bpmn2.MessageFlow;
+import org.obeonetwork.dsl.bpmn2.ReceiveTask;
+import org.obeonetwork.dsl.bpmn2.ScriptTask;
+import org.obeonetwork.dsl.bpmn2.SendTask;
+import org.obeonetwork.dsl.bpmn2.ServiceTask;
+import org.obeonetwork.dsl.bpmn2.Task;
+import org.obeonetwork.dsl.bpmn2.UserTask;
+import org.obeonetwork.dsl.bpmn2.impl.TaskImpl;
+
+/**
+ * Test and conversion services to operate on Task objects.
+ * 
+ * @author vrichard
+ *
+ */
+public class TaskService {
+
+	public String getLabel(Task task, boolean borderedNodes) {
+		String result = " \n \n" + task.getName();
+		// if (borderedNodes) {
+		// result = " \n" + result;
+		// }
+		return result;
+	}
+
+	public String trimLabel(EObject eo, String newLabel) {
+		if (eo instanceof Task) {
+			return newLabel.trim();
+		}
+		return newLabel;
+	}
+
+	public boolean isLikeScriptTask(final EObject eObject) {
+		boolean isLikeScriptTask = eObject instanceof ScriptTask;
+		if (!isLikeScriptTask && eObject instanceof CallActivity) {
+			CallActivity callActivity = (CallActivity) eObject;
+			isLikeScriptTask = callActivity.getCalledElementRef() instanceof GlobalScriptTask;
+		}
+		return isLikeScriptTask;
+	}
+
+	public boolean isLikeBusinessRuleTask(final EObject eObject) {
+		boolean isLikeBusinessRuleTask = eObject instanceof BusinessRuleTask;
+		if (!isLikeBusinessRuleTask && eObject instanceof CallActivity) {
+			CallActivity callActivity = (CallActivity) eObject;
+			isLikeBusinessRuleTask = callActivity.getCalledElementRef() instanceof GlobalBusinessRuleTask;
+		}
+		return isLikeBusinessRuleTask;
+	}
+
+	public boolean isLikeUserTask(final EObject eObject) {
+		boolean isLikeUserTask = eObject instanceof UserTask;
+		if (!isLikeUserTask && eObject instanceof CallActivity) {
+			CallActivity callActivity = (CallActivity) eObject;
+			isLikeUserTask = callActivity.getCalledElementRef() instanceof GlobalUserTask;
+		}
+		return isLikeUserTask;
+	}
+
+	public boolean isLikeManualTask(final EObject eObject) {
+		boolean isLikeManualTask = eObject instanceof ManualTask;
+		if (!isLikeManualTask && eObject instanceof CallActivity) {
+			CallActivity callActivity = (CallActivity) eObject;
+			isLikeManualTask = callActivity.getCalledElementRef() instanceof GlobalManualTask;
+		}
+		return isLikeManualTask;
+	}
+
+	public boolean isLikeTask(final EObject eObject) {
+		return eObject instanceof Task && eObject.getClass().isAssignableFrom(TaskImpl.class);
+	}
+
+	public boolean isLikeReceiveTask(final EObject eObject) {
+		return eObject instanceof ReceiveTask;
+	}
+
+	public boolean isLikeSendTask(final EObject eObject) {
+		return eObject instanceof SendTask;
+	}
+
+	public boolean isLikeServiceTask(final EObject eObject) {
+		return eObject instanceof ServiceTask;
+	}
+
+	public boolean isLikeChoreographyTask(final EObject eObject) {
+		return eObject instanceof ChoreographyTask;
+	}
+
+	public Task convertToTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (!eObject.getClass().isAssignableFrom(TaskImpl.class)) {
+			return (Task) convertToSpecificTask((Task) view.getTarget(), Bpmn2Package.eINSTANCE.getTask());
+		}
+		return eObject;
+	}
+
+	public BusinessRuleTask convertToBusinessRuleTask(final DNodeContainer view) {
+		return (BusinessRuleTask) convertToSpecificTask((Task) view.getTarget(),
+				Bpmn2Package.eINSTANCE.getBusinessRuleTask());
+	}
+
+	public ManualTask convertToManualTask(final DNodeContainer view) {
+		return (ManualTask) convertToSpecificTask((Task) view.getTarget(), Bpmn2Package.eINSTANCE.getManualTask());
+	}
+
+	public ReceiveTask convertToReceiveTask(final DNodeContainer view) {
+		return (ReceiveTask) convertToSpecificTask((Task) view.getTarget(), Bpmn2Package.eINSTANCE.getReceiveTask());
+	}
+
+	public ScriptTask convertToScriptTask(final DNodeContainer view) {
+		return (ScriptTask) convertToSpecificTask((Task) view.getTarget(), Bpmn2Package.eINSTANCE.getScriptTask());
+	}
+
+	public SendTask convertToSendTask(final DNodeContainer view) {
+		return (SendTask) convertToSpecificTask((Task) view.getTarget(), Bpmn2Package.eINSTANCE.getSendTask());
+	}
+
+	public ServiceTask convertToServiceTask(final DNodeContainer view) {
+		return (ServiceTask) convertToSpecificTask((Task) view.getTarget(), Bpmn2Package.eINSTANCE.getServiceTask());
+	}
+
+	public UserTask convertToUserTask(final DNodeContainer view) {
+		return (UserTask) convertToSpecificTask((Task) view.getTarget(), Bpmn2Package.eINSTANCE.getUserTask());
+	}
+
+	private Task convertToSpecificTask(Task task, EClass eClass) {
+		if (eClass.equals(task.eClass())) {
+			return task;
+		} else {
+			Task cloneTask = clone(task, (Task) Bpmn2Factory.eINSTANCE.create(eClass));
+			return cloneTask;
+		}
+	}
+
+	private Task clone(Task task, Task cloneTask) {
+		cloneTask.getBoundaryEventRefs().addAll(task.getBoundaryEventRefs());
+		cloneTask.getCategoryValueRef().addAll(task.getCategoryValueRef());
+		cloneTask.getDataInputAssociations().addAll(task.getDataInputAssociations());
+		cloneTask.getDataOutputAssociations().addAll(task.getDataOutputAssociations());
+		cloneTask.getDocumentation().addAll(task.getDocumentation());
+		cloneTask.getExtensionDefinitions().addAll(task.getExtensionDefinitions());
+		cloneTask.getExtensionValues().addAll(task.getExtensionValues());
+		cloneTask.getIncoming().addAll(task.getIncoming());
+		cloneTask.getIncomingConversationLinks().addAll(task.getIncomingConversationLinks());
+		cloneTask.getLanes().addAll(task.getLanes());
+		cloneTask.getOutgoing().addAll(task.getOutgoing());
+		cloneTask.getOutgoingConversationLinks().addAll(task.getOutgoingConversationLinks());
+		cloneTask.getProperties().addAll(task.getProperties());
+		cloneTask.getResources().addAll(task.getResources());
+
+		cloneTask.setAuditing(task.getAuditing());
+		cloneTask.setCompletionQuantity(task.getCompletionQuantity());
+		cloneTask.setDefault(task.getDefault());
+		cloneTask.setId(task.getId());
+		cloneTask.setIoSpecification(task.getIoSpecification());
+		cloneTask.setIsForCompensation(task.isIsForCompensation());
+		cloneTask.setLoopCharacteristics(task.getLoopCharacteristics());
+		cloneTask.setMonitoring(task.getMonitoring());
+		cloneTask.setName(task.getName());
+		cloneTask.setStartQuantity(task.getStartQuantity());
+
+		updateMessageFlowsAndAssociations(task, cloneTask);
+
+		FlowElementsContainer container = (FlowElementsContainer) task.eContainer();
+		container.getFlowElements().remove(task);
+		container.getFlowElements().add(cloneTask);
+
+		return cloneTask;
+	}
+
+	private void updateMessageFlowsAndAssociations(Task task, Task cloneTask) {
+		ECrossReferenceAdapter eCrossReferenceAdapter = ServiceHelper.getCrossReferenceAdapter(task);
+		if (eCrossReferenceAdapter != null) {
+			Collection<Setting> inverseReferences = eCrossReferenceAdapter.getInverseReferences(task);
+			for (Setting setting : inverseReferences) {
+				EObject object = setting.getEObject();
+				if (object instanceof MessageFlow) {
+					updateMessageFlow(task, cloneTask, (MessageFlow) object);
+				} else if (object instanceof Association) {
+					updateAssociation(task, cloneTask, (Association) object);
+				}
+			}
+		}
+	}
+
+	private void updateMessageFlow(Task task, Task cloneTask, MessageFlow messageFlow) {
+		if (task.equals(messageFlow.getSourceRef())) {
+			messageFlow.setSourceRef(cloneTask);
+		}
+		if (task.equals(messageFlow.getTargetRef())) {
+			messageFlow.setTargetRef(cloneTask);
+		}
+	}
+
+	private void updateAssociation(Task task, Task cloneTask, Association association) {
+		if (task.equals(association.getSourceRef())) {
+			association.setSourceRef(cloneTask);
+		}
+		if (task.equals(association.getTargetRef())) {
+			association.setTargetRef(cloneTask);
+		}
+	}
+
+}
