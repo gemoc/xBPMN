075e43228e70d1024585aa6fc14f14b8add77bc8
Didier Vojtisek <didier.vojtisek@inria.fr>
2021-08-04 18:11:38 +0200

    avoid mixing k3 xtend and xtend  in different packages
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

---
+++ plugins/org.gemoc.xbpmn.k3dsa/src/org/gemoc/xbpmn/k3dsa/commons/NotImplementedException.java
@@ -0,0 +1,15 @@
+package org.gemoc.xbpmn.k3dsa.commons;
+
+
+public class NotImplementedException extends Exception {
+
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NotImplementedException (String string) {
+	  super(string);
+	}
+
+}
